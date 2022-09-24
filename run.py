c9c616a51d491a4404e7ac192ac75f9fd4caba5c
Folarin Ogungbemi <Folarine1@gmail.com>
2022-09-24 13:44:15 +0000

    Set up APIs and create access to kologram google spreadsheet

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+"""
+Constant variable that stores the APIs needed for the program
+"""
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+"""
+Accessing our google spreadsheets data and storing
+in our constant variable for modification
+"""
+CREDS = Credentials.from_service_account_file('creds.json')
+CREDS_SCOPE = CREDS.with_scopes(SCOPE)
+GSPREAD_USER = gspread.authorize(CREDS_SCOPE)
+SHEET = GSPREAD_USER.open('kologram')
+
+income_statement = SHEET.worksheet('income statement')
+
+kolo_data = income_statement.get_all_values()
+print(kolo_data)
