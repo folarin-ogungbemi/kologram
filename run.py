f5ef78d9c2eaf464ddd1fc8a290c3ad40a59ae47
Folarin Ogungbemi <Folarine1@gmail.com>
2022-09-30 23:14:04 +0000

    Add conditional statement to kolo day and update account

--- run.py
+++ run.py
@@ -235,26 +235,28 @@ def kolo_day():
     while True:
         question = input("Would you like to Kolo today (y/n): ")
         if question == 'y'.lower():
-            my_project = list_of_projects()
-            kolo_amount = float(input("Enter contribution amount: "))
-            # Enter date the account was credited
-            credit_date = datetime.date.today()
-            # serialize datetime into JSON
-            date_dump = json.dumps(credit_date, default=str)
-            main_dump = date_dump.strip('"')
-            account = [[main_dump, kolo_amount]]
-            # from Google sheets API
-            S_VALUES.append(spreadsheetId=SHEET_ID,
-                            range=my_project+"!C5:D5",
-                            valueInputOption="USER_ENTERED",
-                            insertDataOption='INSERT_ROWS',
-                            body={"values": account}).execute()
-
-            print("Your koloproject has been updated\n")
+            my_project = project_search()
+            if my_project == "None":
+                print("\nThe project you entered does not exist!!")
+                print("Please check your project list and try again")
+            else:
+                kolo_amount = float(input("Enter contribution amount: "))
+                # Enter date the account was credited
+                credit_date = datetime.date.today()
+                # serialize datetime into JSON
+                date_dump = json.dumps(credit_date, default=str)
+                main_dump = date_dump.strip('"')
+                account = [[main_dump, kolo_amount]]
+                # from Google sheets API
+                S_VALUES.append(spreadsheetId=SHEET_ID,
+                                range=my_project+"!C5:D5",
+                                valueInputOption="USER_ENTERED",
+                                insertDataOption='INSERT_ROWS',
+                                body={"values": account}).execute()
+                print("Your koloproject has been updated\n")
             return False
         if question == 'n'.lower():
             print("Thank you for using kologram")
-            main()
             return False
         print(f"Invalid input: '{question}'. Please type y or n")
 
